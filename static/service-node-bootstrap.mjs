ea73af519b13a5b69920c224f1e8978a69658e04
jorsamj <50445927+jorsamj@users.noreply.github.com>
2026-09-20 13:56:28 +0800

    chore(ui): bust service node runtime cache

--- static/service-node-bootstrap.mjs
+++ static/service-node-bootstrap.mjs
@@ -1,4 +1,4 @@
-import {installServiceNodeRuntime} from './modules/service-node-runtime.js?v=422534';
+import {installServiceNodeRuntime} from './modules/service-node-runtime.js?v=422535';
 
 function boot() {
   if (window.__serviceNodeRuntimeInstalled) return;
